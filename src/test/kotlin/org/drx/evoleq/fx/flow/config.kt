fec57a570d4fd80991e70490fb060d6976135570
Florian Schmidt <florian.schmidt@platis.de>
2019-03-19 16:00:42 +0100

    Added config-file

--- src/test/kotlin/org/drx/evoleq/fx/flow/config.kt
+++ src/test/kotlin/org/drx/evoleq/fx/flow/config.kt
@@ -16,6 +16,10 @@
 package org.drx.evoleq.fx.flow
 
 object Config{
-    val base: String by lazy{ this::class.java.getResource(".").toExternalForm().replace("/classes/", "/resources/") }
+    val base: String by lazy{
+        this::class.java.getResource(".").toExternalForm()
+                .replace("/classes/", "/resources/")
+                .replace("file:/", "/")
+    }
     val style1 = base + "style1.css"
 }
