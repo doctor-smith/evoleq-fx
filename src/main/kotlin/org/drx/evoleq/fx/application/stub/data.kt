5c93a8b414e546665ef1c222a9a5ad0d935e21a8
Florian Schmidt <florian.schmidt@platis.de>
2019-10-19 10:48:36 +0200

    AppMessage carries data

--- src/main/kotlin/org/drx/evoleq/fx/application/stub/data.kt
+++ src/main/kotlin/org/drx/evoleq/fx/application/stub/data.kt
@@ -20,23 +20,23 @@ import org.drx.evoleq.fx.component.FxComponent
 import org.drx.evoleq.stub.ID
 import org.drx.evoleq.stub.Stub
 
-sealed class AppMessage<Data> {
-    sealed class Request<Data> : AppMessage<Data>() {
-        data class ShowStage<Data>(val id: ID) : Request<Data>()
-        data class HideStage<Data>(val id: ID): Request<Data>()
-        data class RegisterStages<Data>(val stages:  ArrayList<Pair<ID, ()-> FxComponent<Stage, Data>>>) : Request<Data>()
+sealed class AppMessage<Data>(open val data: Data) {
+    sealed class Request<Data>(override val data: Data) : AppMessage<Data>(data) {
+        data class ShowStage<Data>(val id: ID, override val data: Data) : Request<Data>(data)
+        data class HideStage<Data>(val id: ID,override val data: Data): Request<Data>(data)
+        data class RegisterStages<Data>(val stages:  ArrayList<Pair<ID, ()-> FxComponent<Stage, Data>>>,override val data: Data) : Request<Data>(data)
     }
-    sealed class Response<Data> : AppMessage<Data>() {
-        data class StageShown<Data>(val stub: Stub<Data>): Response<Data>()
-        data class StageHidden<Data>(val id: ID): Response<Data>()
-        class StagesRegistered<Data> : Response<Data>()
+    sealed class Response<Data>(override val data: Data) : AppMessage<Data>(data) {
+        data class StageShown<Data>(val stub: Stub<Data>, override val data: Data): Response<Data>(data)
+        data class StageHidden<Data>(val id: ID, override val data: Data): Response<Data>(data)
+        class StagesRegistered<Data>(override val data: Data) : Response<Data>(data)
     }
-    sealed class Process<Data> : AppMessage<Data>() {
-        class Start<Data> : Process<Data>()
-        data class DriveStub<Data>(val stub: Stub<Data>, val initialData: Data) : Process<Data>()
-        data class Error<Data>(val stageId: ID, val data: Data, val throwable: Throwable): Process<Data>()
-        class Terminate<Data> : Process<Data>()
-        class Terminated<Data> : Process<Data>()
+    sealed class Process<Data>(override val data: Data) : AppMessage<Data>(data) {
+        class Start<Data>(override val data: Data) : Process<Data>(data)
+        data class DriveStub<Data>(val stub: Stub<Data>, override val data: Data) : Process<Data>(data)
+        data class Error<Data>(val stageId: ID, override val data: Data, val throwable: Throwable): Process<Data>(data)
+        class Terminate<Data>(override val data: Data) : Process<Data>(data)
+        class Terminated<Data>(override val data: Data) : Process<Data>(data)
 
     }
 }
